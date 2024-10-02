145a74374dc52e7342fc1e4e5777f61ebf59a2f5
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-10-02 19:56:26 +0530

    Add files via upload

---
+++ 4 gfg find the sum of all items.py
@@ -0,0 +1,42 @@
+### Python program to find the sum of all items in a dictionary
+###  2nd Oct
+
+Input = {'a': 100, 'b':200, 'c':300}
+sum_= 0
+for i in Input:
+    print(i, " : ", Input[i])
+    sum_ += Input[i]
+
+print(sum_)
+
+# a  :  100
+# b  :  200
+# c  :  300
+# 600
+
+
+# #### 2. Using 'values'
+
+
+def returnSum(dict):
+
+	sum = 0
+	for i in dict.values():
+		sum = sum + i
+
+	return sum
+
+
+# Driver Function
+dict = {'a': 100, 'b': 200, 'c': 300}
+print("Sum :", returnSum(dict))    ## 600
+
+
+############         3           ###########################
+import functools
+
+dic = {'a': 100, 'b': 200, 'c': 300}
+
+sum_dic = functools.reduce(lambda ac,k: ac+dic[k], dic, 0)
+
+print("Sum :", sum_dic)   # Sum : 600 
